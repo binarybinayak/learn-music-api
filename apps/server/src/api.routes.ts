c4aa7b08f662e169b0faa0b3fcbc06038bb9bdd4
binarybinayak <binayaksadhukhan@gmail.com>
2026-02-21 20:20:30 +0530

    Configure cors

--- apps/server/src/api.routes.ts
+++ apps/server/src/api.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import nameThatNoteRouter from "./controller/nameThatNote.controller";
 import matchThePitchRouter from "./controller/matchThePitch.controller";
+import cors from "cors";
 
 const router = Router();
+
+router.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  }),
+);
 
 router.use("/name-that-note", nameThatNoteRouter);
 router.use("/match-the-pitch", matchThePitchRouter);
