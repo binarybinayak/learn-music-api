2639d4257fe42590393d492de25b13522981a797
binarybinayak <binayaksadhukhan@gmail.com>
2026-02-21 11:53:16 +0530

    config dev run script

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import app from "./src/app";
+import chalk from "chalk";
 
 dotenv.config();
 
-
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port " + process.env.PORT);
+  console.log(
+    `\n${chalk.green("➜")}  ${chalk.bold("Local API:")}     ${chalk.cyan(`http://localhost:${process.env.PORT}/`)}\n`,
+  );
 });
